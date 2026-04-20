cc5bb3cf3d82f046f5e41d10c5e6f1b260bb5d59
Eduardo Rodríguez Velázquez <a01739279@tec.mx>
2026-04-20 07:17:17 -0600

    Subir a Github progreso

---
+++ backend/server.js
@@ -0,0 +1,260 @@
+require("dotenv").config();
+const express = require("express");
+const mariadb = require("mariadb");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
+const path = require("path");
+const jwt = require("jsonwebtoken");
+const { decode } = require("punycode");
+
+const SECRET = process.env.JWT_SECRET;
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../frontend"), {
+    index: false
+}));
+
+// conexión a MariaDB
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    connectionLimit: 5
+});
+
+const verifyToken = (req, res, next) => {
+    const authheaders = req.headers["authorization"];
+    const token = authheaders && authheaders.split(" ")[1];
+    if (!token) {
+        return res.status(401).json({
+            status: "error",
+            message: "No autorizado"
+        });
+    }
+    jwt.verify(token, SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).json({
+                status: "error",
+                message: "Sesión expirada"
+            });
+        }
+        req.user = decoded;
+        next();
+    });
+};
+
+const verifyRole = (rolPermitido) => {
+    return (req, res, next) => {
+        if (!req.user) {
+            return res.redirect("/");
+        }
+
+        if (req.user.rol !== rolPermitido) {
+            return res.redirect("/home");
+        }
+
+        next();
+    };
+};
+
+app.get("/verification", verifyToken, (req, res) => {
+    res.json({
+        status: "success",
+        message: "Acceso concedido",
+        usuario: req.user
+    });
+});
+
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    let conn;
+    try {
+        conn = await pool.getConnection();
+
+        const rows = await conn.query(
+            "SELECT * FROM usuarios WHERE email = ?",
+            [email]
+        );
+
+        const user = rows[0];
+
+        if (!user) {
+            return res.json({
+                status: "error",
+                message: "Usuario o contraseña incorrecta"
+            });
+        }
+        
+        const match = await bcrypt.compare(password, user.password);
+
+        if (match) {
+            const token = jwt.sign(
+                {id: user.id, email: user.email, rol: user.rol},
+                SECRET,
+                { expiresIn: '1hr' }
+            );
+
+            return res.json({
+                status: "success",
+                message: "Login correcto",
+                token: token
+            });
+        } else {
+            res.json({
+                status: "error",
+                message: "Usuario o contraseña incorrecta"
+            });
+        }
+    } catch (err) {
+        console.error(err);
+        res.json({
+            status: "error",
+            message: "Error en servidor"
+        });
+    } finally {
+        if (conn) conn.release();
+    }
+});
+
+app.post("/register", async (req, res) => {
+    const { email, password } = req.body;
+
+    let conn;
+    try {
+        conn = await pool.getConnection();
+
+        const existing = await conn.query(
+            "SELECT * FROM usuarios WHERE email = ?",
+            [email]
+        );
+
+        if (existing.length > 0) {
+            return res.json({
+                status: "error",
+                message: "El usuario ya existe"
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const result = await conn.query(
+            "INSERT INTO usuarios (email, password, rol) VALUES (?, ?, 'alumno')",
+            [email, hashedPassword]
+        );
+
+        const token = jwt.sign(
+            {id: Number(result.insertId), email: email, rol: "alumno"},
+            SECRET,
+            {expiresIn: '1hr'}
+        )
+
+        res.json({
+            status: "success",
+            message: "Usuario creado correctamente",
+            token: token
+        });
+
+    } catch (err) {
+        console.error(err);
+        res.json({
+            status: "error",
+            message: "Error al registrar"
+        });
+    } finally {
+        if (conn) conn.release();
+    }
+});
+
+app.post("/api/jugar", verifyToken, async (req, res) => {
+    const { x, y, nivel } = req.body;
+
+    if (!Number.isInteger(x) || !Number.isInteger(y)) {
+        return res.status(400).json({
+            status: "error",
+            message: "Coordenadas inválidas"
+        });
+    }
+
+    let conn;
+    try {
+        conn = await pool.getConnection();
+
+        const rows = await conn.query(
+            "SELECT meta_x, meta_y FROM niveles WHERE id = ?",
+            [nivel]
+        );
+
+        if (rows.length === 0) {
+            return res.status(404).json({
+                status: "error",
+                message: "Nivel no existe"
+            });
+        }
+
+        const meta = rows[0];
+
+        const acierto = (x === meta.meta_x && y === meta.meta_y);
+
+        res.json({
+            status: "success",
+            resultado: acierto ? "acierto" : "fallo"
+        });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            status: "error",
+            message: "Error del servidor"
+        });
+    } finally {
+        if (conn) conn.release();
+    }
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/index.html"));
+});
+
+app.get("/home", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/home.html"));
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/login.html"));
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/register.html"));
+});
+
+app.get("/perfil", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/perfil.html"));
+});
+
+app.get("/mundos", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/mundos.html"));
+});
+
+app.get("/niveles", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/niveles.html"));
+});
+
+app.get("/juego", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/juego.html"));
+});
+
+app.get("/dashboard",(req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dashboard.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor en http://localhost:${PORT}`);
+});
---
+++ frontend/scripts.js
@@ -0,0 +1,308 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    const token = localStorage.getItem("token");
+    const path = window.location.pathname;
+
+    const rutasProtegidas = ["/home", "/perfil", "/mundos", "/niveles", "/juego", "/dashboard"]
+    const esRutaProtegida = rutasProtegidas.some(ruta => path.startsWith(ruta))
+
+    if (esRutaProtegida) {
+        if (!token) {
+            window.location.href = "/";
+            return;
+        }
+        
+        const accesoValido = await verificarAcceso(token);
+
+        if (!accesoValido) {
+            localStorage.removeItem("token");
+            window.location.href = "/";
+            return;
+        }
+
+        if (path.startsWith("/dashboard")) {
+            const payload = JSON.parse(atob(token.split(".")[1]));
+            if (payload.rol !== "profesor") {
+                window.location.href = "/home";
+                return;
+            }
+        }
+    }
+    document.body.style.display = "block";
+});
+
+const loginForm = document.getElementById("loginForm");
+if (loginForm) {
+    loginForm.addEventListener("submit", async function(e) {
+        e.preventDefault();
+    
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        const mensaje = document.getElementById("mensajeServer");
+
+        try {
+            const res = await fetch("http://localhost:3000/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ email, password })
+            });
+
+            const data = await res.json();
+            mensaje.innerText = data.message;
+
+            if (data.status === "success") {
+                localStorage.setItem("token", data.token);
+                mensaje.style.color = "green";
+                mensaje.innerText = "Acceso correcto";
+
+                const payload = JSON.parse(atob(data.token.split(".")[1]));
+
+                setTimeout(() => {
+                    if (payload.rol === "profesor") {
+                        window.location.href = "/dashboard"
+                    } else {
+                        window.location.href = "/home";
+                    }
+                }, 1500);
+            } else {
+                mensaje.innerText = data.message || "Error desconocido";
+                mensaje.style.color = "red";   
+            }
+
+        } catch (error) {
+            alert("Error al conectar con el servidor");
+        }
+    });
+}
+
+const registerForm = document.getElementById("registerForm");
+if (registerForm) {
+    registerForm.addEventListener("submit", async function (e) {
+        e.preventDefault();
+
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        const mensaje = document.getElementById("mensajeServer");
+
+        try {
+            const res = await fetch("http://localhost:3000/register", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ email, password })
+            });
+
+            const data = await res.json();
+
+            if (data.status === "success") {
+                localStorage.setItem("token", data.token);
+
+                mensaje.style.color = "green";
+                mensaje.innerText = "Cuenta creada y sesión iniciada";
+
+                setTimeout(() => {
+                    window.location.href = "/home";
+                }, 1500);
+            } else {
+                mensaje.style.color = "red";
+                mensaje.innerText = data.message;
+            }
+
+        } catch (error) {
+            alert("Error al conectar con el servidor");
+        }
+    });
+}
+
+const canvas = document.getElementById("canvas_grid");
+if(canvas) {
+    const ctx = canvas.getContext("2d");
+    const size = 700;
+    const range = 10; // de -10 a 10
+    const step = size / (range * 2); // tamaño de cada celda
+
+    // Dibujar grid
+    function drawGrid() {
+        ctx.clearRect(0, 0, size, size);
+
+        for (let i = -range; i <= range; i++) {
+            let pos = size / 2 + i * step;
+
+        // líneas verticales
+            ctx.beginPath();
+            ctx.moveTo(pos, 0);
+            ctx.lineTo(pos, size);
+            ctx.strokeStyle = i === 0 ? "black" : "#ccc";
+            ctx.stroke();
+
+        // líneas horizontales
+            ctx.beginPath();
+            ctx.moveTo(0, pos);
+            ctx.lineTo(size, pos);
+            ctx.strokeStyle = i === 0 ? "black" : "#ccc";
+            ctx.stroke();
+        }
+    }
+
+    drawGrid();
+
+    // Temporal (luego vendrá del backend)
+    const puntoCorrecto = { x: 3, y: -2 };
+    const puntos = [];
+
+    function randomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    while (puntos.length < 2) {
+        let p = {
+            x: randomInt(-10, 10),
+            y: randomInt(-10, 10)
+        };
+        if ((p.x !== puntoCorrecto.x || p.y !== puntoCorrecto.y) && !puntos.some(pt => pt.x === p.x && pt.y === p.y)) {
+            puntos.push(p);
+        }
+    }
+
+    puntos.push(puntoCorrecto);
+
+    function drawPoints() {
+        puntos.forEach(p => {
+            const px = size / 2 + p.x * step;
+            const py = size / 2 - p.y * step;
+            ctx.beginPath();
+            ctx.arc(px, py, 6, 0, 2 * Math.PI);
+            ctx.fillStyle = "red";
+            ctx.fill();
+        });
+    }
+
+    drawPoints();
+
+    canvas.addEventListener("click", (e) => {
+        const rect = canvas.getBoundingClientRect();
+
+        const xPixel = e.clientX - rect.left;
+        const yPixel = e.clientY - rect.top;
+
+        // Convertir a coordenadas cartesianas
+        let x = (xPixel - size / 2) / step;
+        let y = (size / 2 - yPixel) / step;
+
+        // Redondear a enteros
+        x = Math.round(x);
+        y = Math.round(y);
+
+        document.getElementById("coords").textContent = `(${x}, ${y})`;
+
+        const clicked = puntos.find(p => p.x === x && p.y === y);
+        if (!clicked) return;
+
+        const token = localStorage.getItem("token");
+
+        fetch("http://localhost:3000/api/jugar", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                x,
+                y,
+                nivel: 1 // Cambiar después para cuando haya más
+            })
+        }).then(res => res.json()).then(data => {
+            if (data.resultado === "acierto") {
+                alert("¡Le diste!");
+            } else {
+                alert("Intenta otra vez");
+            }
+        }).catch(err => console.error(err));
+    });
+}
+
+async function verificarAcceso(token) {
+    try {
+        const res = await fetch("http://localhost:3000/verification", {
+            method: "GET", 
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            }
+        });
+
+        if (res.status === 401 || res.status === 403) {
+            return false;
+        }
+        
+        const data = await res.json();
+        
+        if (data.status === "success") {
+            const bienvenida = document.getElementById("bienvenidaUsuario")
+            if (bienvenida) {
+                bienvenida.innerText = `Bienvenido ${data.usuario.email}`;
+            }
+            return true
+        } else {
+            return false
+        }
+
+    } catch (error) {
+        console.error("Error de autenticazión", error);
+        return false
+    }
+};
+
+function cerrarSesion() {
+    const confirmar = confirm("¿Seguro que quieres cerrar sesión?");
+
+    if (confirmar) {
+        localStorage.removeItem("token");
+        window.location.href = "/";   
+    }
+};
+
+function irInicioSesion() {
+    const token = localStorage.getItem("token");
+    
+    if (token) {
+        const payload = JSON.parse(atob(token.split(".")[1]));
+        if (payload.rol === "profesor") {
+            window.location.href = "/dashboard";
+        } else {
+            window.location.href = "/home";
+        }
+    } else {
+        window.location.href = "/login"
+    }
+};
+
+function irCrearCuenta() {
+    window.location.href = "/register"
+};
+
+function irPaginaInicio() {
+    window.location.href = "/"
+};
+
+function irPerfil() {
+    window.location.href = "/perfil"
+}
+
+function irAventura() {
+    window.location.href = "/mundos"
+}
+
+function irNiveles() {
+    window.location.href = "/niveles"
+}
+
+function irJuego() {
+    window.location.href = "/juego"
+}
+
+function regresarHome() {
+    window.location.href = "/home"
+}
